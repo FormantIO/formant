afc4a336ca4bc122866bc2c08baa57d07814edf7
Formant CI <eng+ci@formant.io>
2022-05-05 21:47:29 +0000

    release 1.79.23

--- examples/python/formant_module/agent/send_image_datapoint.py
+++ examples/python/formant_module/agent/send_image_datapoint.py
@@ -4,7 +4,7 @@ from formant.sdk.agent.v1 import Client as FormantClient
 if __name__ == "__main__":
     fclient = FormantClient()
 
-    image = cv2.imread("/path/to/example.jpg")
-    encoded = cv2.imencode(".jpg", image)[1].tostring()
+    image = cv2.imread("../../../data/example.jpg")
+    encoded = cv2.imencode(".jpg", image)[1].tobytes()
 
     fclient.post_image("example.image", encoded)
