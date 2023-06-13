5470826f5dd7a54197c49f0d080b694a94e113f8
Formant CI <eng+ci@formant.io>
2023-06-13 01:16:59 +0000

    release 1.128.33

--- examples/python/formant_module/agent/intervention.py
+++ examples/python/formant_module/agent/intervention.py
@@ -3,9 +3,11 @@ import time
 from formant.sdk.agent.v1 import Client as FormantClient
 
 
-IMAGE_PATH = os.path.join(os.getcwd(), "my_image.jpg")
-IMAGE_TYPE = "image/jpg"
-SEVERITY_TYPE = "warning"
+# The image that is displayed with the request
+# (use a local file for the purpose of example)
+IMAGE_PATH = os.path.join(os.getcwd(), "example_image.png")
+IMAGE_TYPE = "image/png"
+SEVERITY_TYPE = "info"
 
 fclient = FormantClient()
 
@@ -13,7 +15,7 @@ def selection_request():
     print("Sending selection intervention request")
     response = fclient.create_selection_intervention_request(
         title="Identify Color",
-        instruction="Select the color of the pallet in the picker.",
+        instruction="What color is the big robot?",
         options=["Pink", "White", "Orange", "Blue"],
         hint=0,
         url=IMAGE_PATH,
@@ -21,6 +23,12 @@ def selection_request():
         severity=SEVERITY_TYPE
     )
     response_id = response.id
+
+    # Optionally ingest the link to the intervention request, which can be used
+    # as an event trigger
+    url = "app.formant.io/events/%s" % response_id
+    fclient.post_text("intervention_url", url)
+
     intervention_response = fclient.get_intervention_response(response_id)
     print("Selection received:\n%s" % intervention_response.selection_response.value)
 
@@ -28,13 +36,17 @@ def labeling_request():
     print("Sending labeling intervention request")
     response = fclient.create_labeling_intervention_request(
         title="Locate and Label Object",
-        instruction="Draw a box around the object and select its type.",
-        labels={"Label1": "Label1Value", "Label2": "Label2Value"},
+        instruction="Draw a box around the big robot and select its type.",
+        labels={"Label1": "Sweeper", "Label2": "Mover"},
         url=IMAGE_PATH,
         content_type=IMAGE_TYPE,
         severity=SEVERITY_TYPE
     )
     response_id = response.id
+
+    url = "app.formant.io/events/%s" % response_id
+    fclient.post_text("intervention_url", url)
+
     intervention_response = fclient.get_intervention_response(response_id)
     print("Labeling received:\n%s" % intervention_response.labeling_response.value)
 
